2c633121a5205a24f686d906af6bdf8a5ea8a0db
Ahmad Shela <ahmedsh717@gmail.com>
2020-10-12 16:52:48 +0300

    Updates

---
+++ precache-manifest.18d14c71096ac77d6352a8eb1f7320f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f366a85835e4fd25deaf0a82ca07579f",
+    "url": "/%7BRESTy%7D/index.html"
+  },
+  {
+    "revision": "afb94afa7ab9dd245f24",
+    "url": "/%7BRESTy%7D/static/css/main.cb69d847.chunk.css"
+  },
+  {
+    "revision": "64909891a31834ed1312",
+    "url": "/%7BRESTy%7D/static/js/2.25b57a3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/%7BRESTy%7D/static/js/2.25b57a3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afb94afa7ab9dd245f24",
+    "url": "/%7BRESTy%7D/static/js/main.cc548428.chunk.js"
+  },
+  {
+    "revision": "098a22f896e314222329",
+    "url": "/%7BRESTy%7D/static/js/runtime-main.1ae28bb0.js"
+  }
+]);
